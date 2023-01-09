ae98b80fc38fa6e0f5372cbfec42a167e5aa5dc2
gerya <gerya@PcGeryy>
2023-01-09 18:44:42 +0100

    Hola esto es el segundo intento

---
+++ JavaApplication19/src/javaapplication19/NewMain.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication19;
+
+/**
+ *
+ * @author gerya
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int mireias;
+        //Hola Marc, vamos a por ti, pero Islandia esta muy caro.
+        
+    }
+    
+}
